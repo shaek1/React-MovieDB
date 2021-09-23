968801fb35d0a3d27fd28bec9345aa0374803e0a
shaek1 <shaeka8@gmail.com>
2021-09-23 21:46:52 +1000

    Load More Button added and styled

--- src/components/Header/Home.js
+++ src/components/Header/Home.js
@@ -7,43 +7,56 @@ import Grid from '../Grid';
 import Thumb from '../Thumb';
 import Spinner from '../Spinner';
 import SearchBar from '../SearchBar';
+import Button from '../Button';
 // Hook
 import { useHomeFetch } from '../../hooks/useHomeFetch';
 // Image
 import NoImage from '../../images/no_image.jpg';
 
 const Home = () => {
-    const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
-
+    const {
+      state,
+      loading,
+      error,
+      searchTerm,
+      setSearchTerm,
+      setIsLoadingMore
+    } = useHomeFetch();
+  
     console.log(state);
-
+  
+    if (error) return <div>Something went wrong ...</div>;
+  
     return (
-        <>
-            {state.results[0] ? (
-                <HeroImage
-                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-                    title={state.results[0].original_title}
-                    text={state.results[0].overview}
-                />
-            ) : null}
-            <SearchBar setSearchTerm={setSearchTerm} />
-            <Grid header={searchTerm ? 'Lek Shuuu, like any of these?' : 'Popular Movies'}>
-                {state.results.map(movie => (
-                    <Thumb
-                    key={movie.id}
-                    clickable
-                    image={
-                        movie.poster_path
-                        ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-                        : NoImage
-                    }
-                    movieId={movie.id}
-                    />
-                ))}
-            </Grid>
-            <Spinner />
-        </>
+      <>
+        {!searchTerm && state.results[0] ? (
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+          />
+        ) : null}
+        <SearchBar setSearchTerm={setSearchTerm} />
+        <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+          {state.results.map(movie => (
+            <Thumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                  ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                  : NoImage
+              }
+              movieId={movie.id}
+            />
+          ))}
+        </Grid>
+        {loading && <Spinner />}
+        {state.page < state.total_pages && !loading && (
+          <Button text='Load More' callback={() => setIsLoadingMore(true)} />
+        )}
+      </>
     );
-};
-
-export default Home;
+  };
+  
+  export default Home;
