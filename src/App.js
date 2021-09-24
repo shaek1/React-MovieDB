b5812b8360fda9a0d0ae930b1188b7b8474fb37f
shaek1 <shaeka8@gmail.com>
2021-09-24 22:33:39 +1000

    Login + Rater added. COMPLETE

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import Header from './components/Header';
 import Home from './components/Header/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import Login from './components/Login';
+// Context
+import UserProvider from './context';
 // Styles
 import { GlobalStyle } from './GlobalStyle';
 
 const App = () =>  (
     <Router>
+      <UserProvider>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/:movieId' element={<Movie />} />
         <Route path='/*' element={<NotFound />} />
       </Routes>
       <GlobalStyle />
+      </UserProvider>
     </Router>
   );
 
