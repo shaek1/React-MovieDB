011d5456dc86899b8f8ac0586fa2d542193a21ab
shaek1 <shaeka8@gmail.com>
2021-09-24 11:56:06 +1000

    PropTypes added to ensure correct input

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // Components
 import Thumb from '../Thumb';
 // Config
@@ -42,5 +43,9 @@ const MovieInfo = ({ movie }) => (
 </Wrapper>
 
 );
+
+MovieInfo.propTypes = {
+    movie: PropTypes.object
+};
 
 export default MovieInfo;
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 // Image
 import searchIcon from '../../images/search-icon.svg'
 // Styles
@@ -35,6 +36,10 @@ const SearchBar = ({ setSearchTerm }) => {
             </Content>
         </Wrapper>
     );
+};
+
+SearchBar.propTypes= {
+    callback: PropTypes.func
 };
 
 export default SearchBar;
